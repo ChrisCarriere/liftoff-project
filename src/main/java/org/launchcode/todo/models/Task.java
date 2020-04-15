f64862453b6c8e9d9a7e44dc2a16a8c9e1c272aa
Chris Carriere <chris.carriere323@gmail.com>
2020-04-15 13:27:40 -0500

    added ids to user/tasks

--- src/main/java/org/launchcode/todo/models/Task.java
+++ src/main/java/org/launchcode/todo/models/Task.java
@@ -4,9 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.Objects;
 
 @Entity
 public class Task extends AbstractEntity {
+
+    private int id;
+    private static int nextId = 1;
 
     @NotBlank
     @Size(min = 3, max = 50, message = "name must be between 3 and 50 characters")
@@ -16,6 +20,8 @@ public class Task extends AbstractEntity {
     public Task(String name, Number priority){
         this.name = name;
         this.priority = priority;
+        this.id = nextId;
+        nextId++;
     }
 
     public Task(){}
@@ -34,5 +40,24 @@ public class Task extends AbstractEntity {
 
     public void setPriority(Number priority) {
         this.priority = priority;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
     }
 }
--- src/main/java/org/launchcode/todo/models/User.java
+++ src/main/java/org/launchcode/todo/models/User.java
@@ -5,9 +5,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
 public class User extends AbstractEntity {
+
+    private int id;
+    private static int nextId = 1;
 
     @NotNull
     private String username;
@@ -21,6 +25,8 @@ public class User extends AbstractEntity {
     public User(String username, String password) {
         this.username = username;
         this.pwHash = encoder.encode(password);
+        this.id = nextId;
+        nextId++;
     }
 
     public String getUsername() {
@@ -31,4 +37,22 @@ public class User extends AbstractEntity {
         return encoder.matches(password, pwHash);
     }
 
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
 }
